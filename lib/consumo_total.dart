cf48428017dbd3a0b838016cfeffc8994273bc9d
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2020-08-29 20:51:13 -0300

    Feature: Total Divion Logic And Front-End

--- lib/consumo_total.dart
+++ lib/consumo_total.dart
@@ -7,17 +7,30 @@ class ConsumoTotal extends StatefulWidget {
 }
 
 class ConsumoTotalState extends State<ConsumoTotal> {
-  final form_consumo = GlobalKey<FormState>();
-  final total_value  = TextEditingController();
+  /* CONTROLLERS */
+  final form_consumo         = GlobalKey<FormState>();
+  final total_value          = TextEditingController();
+  final total_people         = TextEditingController();
+  final total_service_charge = TextEditingController();
 
   @override
   void dispose() {
     total_value.dispose();
+    total_people.dispose();
+    total_service_charge.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    var orangeColor     = new Color(0xFFF9AC18);
+    double total        = 0;
+    double custo        = 0;
+    int pessoas         = 0;
+    double taxa         = 0;
+    double porcentagem  = 0;
+    String result;
+
     return MaterialApp(
       home: Scaffold(
         /* Função que preenche os espaços da tela, barra de cima... */
@@ -31,11 +44,29 @@ class ConsumoTotalState extends State<ConsumoTotal> {
           padding: const EdgeInsets.all(40.0),
           child: new Column(
             children: [
-              Text('Vamos dividir!'),
+              /* PAGE TITLE */
+              Text(
+                'Vamos Dividir!',
+                style: TextStyle(fontSize: 26, fontFamily: 'Poppins'),
+                textAlign: TextAlign.center
+              ),
+              
+              /* DIVSION FORM */
               Form(
                 key: form_consumo,
                 child: Column(
                   children: <Widget>[
+                    /* MONEY ICON */
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Icon(
+                        Icons.monetization_on,
+                        color: Colors.green,
+                        size: 24,
+                      ),
+                    ),
+                    
+                    /* INPUT TOTAL VALUE */
                     TextFormField(
                       controller: total_value,
                       decoration: new InputDecoration(
@@ -46,7 +77,22 @@ class ConsumoTotalState extends State<ConsumoTotal> {
                         return (value.isEmpty) ? 'Por favor insira um valor' : null;
                       }
                     ),
+
+                    Padding(padding: new EdgeInsets.fromLTRB(0.0,15.0,0.0,0.0,)),
+
+                    /* PEOPLE ICON */
+                    Container(
+                      alignment: Alignment.bottomLeft,
+                      child: Icon(
+                        Icons.insert_emoticon,
+                        color: Colors.blue,
+                        size: 24,
+                      ),
+                    ),
+
+                    /* INPUT AMOUNT OF PEOPLE */
                     TextFormField(
+                      controller: total_people,
                       decoration: new InputDecoration(
                         hintText: 'Digite a quantidade...', labelText: 'Quantidade de pessoas',
                       ),
@@ -55,21 +101,77 @@ class ConsumoTotalState extends State<ConsumoTotal> {
                         return (value.isEmpty) ? 'Por favor insira a quantidade' : null;
                       }
                     ),
+
+                    Padding(padding: new EdgeInsets.fromLTRB(0.0,15.0,0.0,0.0,)),
+
+                    /* PEOPLE ICON */
+                    Container(
+                      alignment: Alignment.bottomLeft,
+                      child: Icon(
+                        Icons.local_bar,
+                        color: Colors.pink,
+                        size: 24,
+                      ),
+                    ),
+
+                    /* INPUT SERVICE CHARGE */
                     TextFormField(
+                      controller: total_service_charge,
                       decoration: new InputDecoration(
-                        hintText: 'Digite a taxa...', labelText: 'Taxa de serviço',
+                        hintText: 'Digite a taxa...', labelText: 'Taxa de serviço (porcentagem)',
                       ),
                       keyboardType: TextInputType.number,
                       validator: (value) {
                         return (value.isEmpty) ? 'Por favor insira a taxa' : null;
                       }
                     ),
-                    RaisedButton(
-                      child: Text('Calcular'),
-                      onPressed: () {
-                        if (form_consumo.currentState.validate())
-                          print(total_value.text);
-                      },
+
+                    Padding(padding: new EdgeInsets.fromLTRB(0.0,20.0,0.0,0.0,)),
+
+                    /* CALCULATE BUTTON */
+                    ButtonTheme(
+                      minWidth: 230.0,
+                      height: 60.0,
+                      child: RaisedButton(
+                        child: Text('Calcular', style: TextStyle(fontSize: 16, fontFamily: 'Poppins'),),
+                        color: orangeColor,
+                        onPressed: () {
+                          if (form_consumo.currentState.validate())
+                            custo = double.parse(total_value.text);
+                            pessoas  = int.parse(total_people.text);
+                            taxa  = double.parse(total_service_charge.text);
+
+                            porcentagem = custo * (taxa / 100);
+                            total = (custo + porcentagem) / pessoas;
+
+                            result = total.toStringAsFixed(2);
+
+                            return showDialog <void>(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Total:'),
+                                  content: SingleChildScrollView(
+                                    child: ListBody(
+                                      children: <Widget>[
+                                        Text('R\u0024$result para cada um.'),
+                                      ],
+                                    ),
+                                  ),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      child: Text('Ok'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                        },
+                      ),
                     ),
                   ],
                 )
